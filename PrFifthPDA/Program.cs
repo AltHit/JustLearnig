8b5d41b8d6b67065f53bfd029aa36f3bf0f57585
AltHit <Daniilprosk4@yandex.ru>
2023-12-15 11:14:25 +0700

    1.4 and 1.5 done

--- PrFifthPDA/Program.cs
+++ PrFifthPDA/Program.cs
@@ -1,6 +1,9 @@
-﻿//#1
+﻿using System.Globalization;
+using System.Text;
+
+//#1
 //Input is a numsTask1.txt file with int numbers. Find the product of elements after minimal
-/*
+
 StreamReader numsTask1 = new("numsTask1.txt");
 
 string[] firInput = numsTask1.ReadToEnd().Split();
@@ -23,7 +26,7 @@ for(int i = firMinIndex + 1; i < firInput.Length; i++)
 }
 
 Console.WriteLine($"Prod of all numbers after minimal is {firProd}");
-*/
+
 
 //#2
 //Input is a numsTask2.txt file with float numbers divided with ;. Sort numbers and write sorted numbers back into file
@@ -31,19 +34,120 @@ Console.WriteLine($"Prod of all numbers after minimal is {firProd}");
 StreamReader numsTask2 = new("numsTask2.txt");
 
 string[] secInput = numsTask2.ReadToEnd().Split(";");
-double secSum = 0;
-
-foreach (string inp in secInput)
+double[] secConverted = new double[secInput.Length];
+numsTask2.Close();
+for (int i = 0; i < secInput.Length; i++)
 {
-    if (Convert.ToDouble(inp) == 0)
-    {
-        break;
-    }
+    secConverted[i] = Convert.ToDouble(secInput[i]);
+}
 
-    if (Convert.ToDouble(inp) > 0)
+Array.Sort(secConverted);
+string secText = "";
+
+foreach (double x in secConverted)
+{
+    secText = secText + Convert.ToString(x, CultureInfo.CurrentCulture) + ";";
+}
+
+await using (FileStream secOut = new FileStream("numsTask2.txt", FileMode.OpenOrCreate))
+{
+    byte[] buffer = Encoding.Default.GetBytes(secText);
+    await secOut.WriteAsync(buffer, 0, buffer.Length);
+}
+
+
+//#3
+//Input is a numsTask3.txt file with ints. Find average number before minimal element
+
+StreamReader numsTask3 = new("numsTask3.txt");
+
+string[] thirdInput = numsTask3.ReadToEnd().Split();
+int thirdAvg = 0,
+    thirdMin = Convert.ToInt32(thirdInput[0]),
+    thirdMinIndex = 0;
+
+for(int i = 0; i < thirdInput.Length; i++)
+{
+    if (Convert.ToInt32(thirdInput[i]) < thirdMin)
     {
-        secSum += Convert.ToDouble(inp);
+        thirdMin = Convert.ToInt32(thirdInput[i]);
+        thirdMinIndex = i;
     }
 }
 
-Console.WriteLine($"Sum of all positive numbers is {secSum}");
+for(int i = 0; i < thirdMinIndex; i++)
+{
+    thirdAvg += Convert.ToInt32(thirdInput[i]);
+}
+
+Console.WriteLine($"Average before minimal element is {thirdAvg/thirdMinIndex}");
+
+
+//#4
+//Input is a numsTask4.txt with ints. Find sum of all elements different from maximal by 1
+
+StreamReader numsTask4 = new("numsTask4.txt");
+
+string[] fourthInput = numsTask4.ReadToEnd().Split();
+int fourthSum = 0,
+    fourthMax = Convert.ToInt32(fourthInput[0]);
+
+foreach (var t in fourthInput)
+{
+    if (Convert.ToInt32(t) > fourthMax)
+    {
+        fourthMax = Convert.ToInt32(t);
+    }
+}
+
+foreach (var t in fourthInput)
+{
+    if (Math.Abs(fourthMax - Convert.ToInt32(t)) >= 1)
+    {
+        fourthSum += Convert.ToInt32(t);
+    }
+}
+
+Console.WriteLine($"Sum of all differentiating elements is {fourthSum}");
+
+
+//#5
+//Input is again numsTask5.txt with ints. Find average between minimal and maximal elements
+
+StreamReader numsTask5 = new("numsTask5.txt");
+
+string[] fifthInput = numsTask5.ReadToEnd().Split();
+int fifthAvg = 0,
+    fifthMax = Convert.ToInt32(fifthInput[0]),
+    fifthMin = Convert.ToInt32(fifthInput[0]),
+    fifthMaxIndex = 0,
+    fifthMinIndex = 0;
+
+for(int i = 0; i < fifthInput.Length; i++)
+{
+    if (Convert.ToInt32(fifthInput[i]) < fifthMin)
+    {
+        fifthMin = Convert.ToInt32(fifthInput[i]);
+        fifthMinIndex = i;
+    }
+}
+
+for(int i = 0; i < fifthInput.Length; i++)
+{
+    if (Convert.ToInt32(fifthInput[i]) > fifthMax)
+    {
+        fifthMax = Convert.ToInt32(fifthInput[i]);
+        fifthMaxIndex = i;
+    }
+}
+
+for(int i = fifthMinIndex + 1; i < fifthMaxIndex; i++)
+{
+    fifthAvg += Convert.ToInt32(fifthInput[i]);
+}
+
+Console.WriteLine(fifthMinIndex);
+Console.WriteLine(fifthMaxIndex);
+Console.WriteLine(fifthAvg);
+
+Console.WriteLine($"Sum of all differentiating elements is {fifthAvg/(fifthMaxIndex - fifthMinIndex - 1)}");
--- PrFourthPDA/Program.cs
+++ PrFourthPDA/Program.cs
@@ -1,17 +1,17 @@
 ﻿//#1
-//Input is a positive number n. Find product of natural numbers multiples to 3 and beyond n
-/*
+//Input is a positive number n. Find product of natural numbers multiples to 3 and below n
+
 Console.WriteLine("Enter the N-number");
 int n = Convert.ToInt32(Console.ReadLine());
 int prod = 1;
 for (int i = 3; i <= n; i += 3)
     prod *= i;
 Console.WriteLine(prod);
-*/
+
 
 //#2
 //Input is a numsTask2.txt file with float numbers divided with ;. Find the sum of all positive numbers until finding a zero
-/*
+
 StreamReader numsTask2 = new("numsTask2.txt");
 
 string[] secInput = numsTask2.ReadToEnd().Split(";");
@@ -31,11 +31,11 @@ foreach (string inp in secInput)
 }
 
 Console.WriteLine($"Sum of all positive numbers is {secSum}");
-*/
+
 
 //#3
 //Input is a numsTask3.txt file with int numbers divided with ",". Find the ratio of maximal and minimal numbers until finding a zero
-/*
+
 StreamReader numsTask3 = new("numsTask3.txt");
 
 string[] thirdInput = numsTask3.ReadToEnd().Split(",");
@@ -64,11 +64,11 @@ foreach (string inp in thirdInput)
 }
 //Console.WriteLine($"Just before min is {thirdMin} and max is {thirdMax}");
 Console.WriteLine($"Ratio between maximal and minimal elements equals {thirdMin / thirdMax}");
-*/
+
 
 //#4
 //Input is a numsTask4.txt file with int numbers divided with spaces. Find the number of similar numbers neighbouring each other
-/*
+
 StreamReader numsTask4 = new("numsTask4.txt");
 
 string[] fourthInput = numsTask4.ReadToEnd().Split();
@@ -83,17 +83,17 @@ for (int i = 1; i < fourthInput.Length; i++)
 }
 
 Console.WriteLine($"There are {fourthSim} similar numbers near by");
-*/
+
 
 //#5
 //Input is 2 whole numbers a and b. Find if a point [a,b] is on area [-1,4;3,-2]
-/*
-Console.WriteLine("Enter number a");
-double as = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Enter number b");
-double bs = Convert.ToDouble(Console.ReadLine());
 
-if(as is >= -1 and <= 3 && bs is <= 4 and >= -2)
+Console.WriteLine("Enter number a");
+double xs = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number b");
+double ys = Convert.ToDouble(Console.ReadLine());
+
+if(xs is >= -1 and <= 3 && ys is <= 4 and >= -2)
 {
     Console.WriteLine("Point IS in range");
 }
@@ -101,7 +101,7 @@ else
 {
     Console.WriteLine("Point IS NOT in range");
 }
-*/
+
 
 //#6
 //Input is 2 whole numbers a and b. Find if a point [a,b] is on area of a triangle
@@ -110,4 +110,10 @@ Console.WriteLine("Enter number a");
 double at = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Enter number b");
 double bt = Convert.ToDouble(Console.ReadLine());
+
+if (((bt - 2) / 2.5f) <= at && ((bt - 2) / -2.5f) >= at && bt is >= -3 and <= 2)
+{
+    Console.WriteLine("Point is INSIDE area");
+}
+else{Console.WriteLine("Point is OUTSIDE area");}
 
